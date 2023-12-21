8385119da3549e25f46bdefdc34dafda2e04c96e
slothsoft <admin@slothsoft.de>
2023-12-21 10:49:36 +0100

    add part 1 of day 20 of 2023 📶

---
+++ 2023/20/PulsePropagation.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.day20;
+
+/// <summary>
+/// <a href="https://adventofcode.com/2023/day/20">Day 20: Pulse Propagation</a>
+/// </summary>
+public class PulsePropagation {
+    public interface IModule {
+        string DisplayName { get; }
+        string[] TargetModules { get; }
+        Signal LastSignal { get; }
+
+        void ReceiveSignal(ModuleContext context, Signal inputSignal);
+    }
+
+    public record BroadcasterModule(string DisplayName, params string[] TargetModules) : IModule {
+        public Signal LastSignal { get; private set; } = Signal.Low;
+
+        public void ReceiveSignal(ModuleContext context, Signal inputSignal) {
+            LastSignal = inputSignal;
+            context.SendSignal(TargetModules, LastSignal);
+        }
+    }
+
+    public record FlipFlopModule(string DisplayName, params string[] TargetModules) : IModule {
+        public Signal LastSignal { get; private set; } = Signal.Low;
+
+        public void ReceiveSignal(ModuleContext context, Signal inputSignal) {
+            if (inputSignal == Signal.Low) {
+                LastSignal = LastSignal == Signal.High ? Signal.Low : Signal.High;
+                context.SendSignal(TargetModules, LastSignal);
+            }
+        }
+    }
+
+    public record ConjunctionModule(string DisplayName, params string[] TargetModules) : IModule {
+        public Signal LastSignal { get; set; } = Signal.Low;
+
+        public void ReceiveSignal(ModuleContext context, Signal inputSignal) {
+            LastSignal = Signal.Low;
+            foreach (var model in context.FetchInputFor(DisplayName)) {
+                if (model.LastSignal == Signal.Low) {
+                    LastSignal = Signal.High;
+                    break;
+                }
+            }
+
+            context.SendSignal(TargetModules, LastSignal);
+        }
+    }
+
+    internal record RxModule : IModule {
+        public string DisplayName => MODULE_RX;
+        public Signal LastSignal { get; private set; } = Signal.None;
+        public string[] TargetModules { get; } = Array.Empty<string>();
+
+        public void ReceiveSignal(ModuleContext context, Signal inputSignal) {
+            if (inputSignal == Signal.Low) {
+                LastSignal = inputSignal;
+            }
+        }
+    }
+    
+    public enum Signal {
+        None,
+        Low,
+        High,
+    }
+
+    public record ModuleContext(IDictionary<string, IModule> Modules) {
+        private readonly IList<Action> _signalPipeline = new List<Action>();
+
+        public long LowSignalsSend { get; private set; }
+        public long HighSignalsSend { get; private set; }
+
+        public void SendSignal(string[] targetModules, Signal inputSignal) {
+            foreach (var targetModule in targetModules) {
+                SendSignal(targetModule, inputSignal);
+            }
+        }
+
+        public void SendSignal(string targetModule, Signal inputSignal) {
+            _signalPipeline.Add(() => {
+                Modules[targetModule].ReceiveSignal(this, inputSignal);
+            });
+
+            if (inputSignal == Signal.Low) {
+                LowSignalsSend++;
+            }
+
+            if (inputSignal == Signal.High) {
+                HighSignalsSend++;
+            }
+        }
+
+        public IEnumerable<IModule> FetchInputFor(string displayName) {
+            return Modules.Select(m => m.Value).Where(m => m.TargetModules.Contains(displayName));
+        }
+
+        public void ExecuteSignalPipeline() {
+            while (_signalPipeline.Count > 0) {
+                var signalSender = _signalPipeline.First();
+                _signalPipeline.RemoveAt(0);
+                signalSender();
+            }
+        }
+    }
+
+    private const string MODULE_BROADCASTER = "broadcaster";
+    private const string MODULE_RX = "rx";
+
+    public PulsePropagation(IEnumerable<string> input) {
+        Modules = ParseInput(input);
+        Modules.Add(MODULE_RX, new RxModule());
+    }
+
+    internal IDictionary<string, IModule> Modules { get; }
+
+    internal static IDictionary<string, IModule> ParseInput(IEnumerable<string> input) {
+        return input.Select(ParseInputLine).ToDictionary(m => m.DisplayName, m => m);
+    }
+
+    private static IModule ParseInputLine(string input) {
+        var split = input.Split(" -> ");
+        var targetModules = split[1].Split(", ");
+
+        if (split[0].Equals(MODULE_BROADCASTER)) {
+            return new BroadcasterModule(split[0], targetModules);
+        }
+
+        switch (split[0][0]) {
+            case '%':
+                return new FlipFlopModule(split[0][1..], targetModules);
+            case '&':
+                return new ConjunctionModule(split[0][1..], targetModules);
+            default:
+                throw new ArgumentException($"Cannot handle parsing of module {split[0]}");
+        }
+    }
+
+    public (long LowSignalsSend, long HighSignalsSend) PushButton() {
+        var context = new ModuleContext(Modules);
+        context.SendSignal(MODULE_BROADCASTER, Signal.Low);
+        context.ExecuteSignalPipeline();
+        return (context.LowSignalsSend, context.HighSignalsSend);
+    }
+
+    public long PushButtonOften(int times) {
+        var (lowSignalsSend, highSignalsSend) = (0L, 0L);
+        for (var i = 0; i < times; i++) {
+            var result = PushButton();
+            lowSignalsSend += result.LowSignalsSend;
+            highSignalsSend += result.HighSignalsSend;
+        }
+
+        return lowSignalsSend * highSignalsSend;
+    }
+
+    public long PushButtonUntilSand() {
+        long result = 0;
+        var context = new ModuleContext(Modules);
+
+        do {
+            result++;
+
+            context.SendSignal(MODULE_BROADCASTER, Signal.Low);
+            context.ExecuteSignalPipeline();
+        } while (Modules[MODULE_RX].LastSignal != Signal.Low);
+            
+        return result;
+    }
+}
---
+++ 2023/20/PulsePropagationTest.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Broadcaster = AoC.day20.PulsePropagation.BroadcasterModule;
+using Conjunction = AoC.day20.PulsePropagation.ConjunctionModule;
+using FlipFlop = AoC.day20.PulsePropagation.FlipFlopModule;
+using IModule = AoC.day20.PulsePropagation.IModule;
+using Signal = AoC.day20.PulsePropagation.Signal;
+
+namespace AoC.day20;
+
+public class PulsePropagationTest {
+    internal record TestModule(string DisplayName, params string[] TargetModules) : IModule {
+        public Signal LastSignal { get; set; } = Signal.Low;
+        public string[] TargetModules { get; set; } = TargetModules;
+
+        public void ReceiveSignal(PulsePropagation.ModuleContext context, Signal inputSignal) {
+            LastSignal = inputSignal;
+        }
+    }
+
+    private const string MODULE_A = "a";
+    private const string MODULE_B = "b";
+    private const string MODULE_C = "c";
+
+    private IDictionary<string, IModule> _models = new Dictionary<string, IModule>();
+    private PulsePropagation.ModuleContext _context = new(new Dictionary<string, IModule>());
+
+    [SetUp]
+    public void SetUp() {
+        _models = new Dictionary<string, IModule>();
+        _context = new PulsePropagation.ModuleContext(_models);
+        
+        _models.Add(MODULE_A, new TestModule(MODULE_A));
+        _models.Add(MODULE_B, new TestModule(MODULE_B));
+        _models.Add(MODULE_C, new TestModule(MODULE_C));
+    }
+
+    [Test]
+    [TestCase(Signal.Low, Signal.Low)]
+    [TestCase(Signal.High, Signal.High)]
+    public void Example1_Broadcaster(Signal inputSignal, Signal outputSignal) {
+        var module = new Broadcaster("BREADCASTER", MODULE_A, MODULE_B);
+        module.ReceiveSignal(_context, inputSignal);
+
+        Assert.AreEqual(outputSignal, module.LastSignal);
+        
+        // this module moves the signal to the targets
+        _context.ExecuteSignalPipeline();
+        Assert.AreEqual(outputSignal, _models[MODULE_A].LastSignal);
+        Assert.AreEqual(outputSignal, _models[MODULE_B].LastSignal);
+    }
+
+    [Test]
+    [TestCase(new Signal[0], Signal.Low)]
+    [TestCase(new[] {Signal.Low}, Signal.High)]
+    [TestCase(new[] {Signal.High}, Signal.Low)]
+    [TestCase(new[] {Signal.Low, Signal.Low}, Signal.Low)]
+    [TestCase(new[] {Signal.Low, Signal.High}, Signal.High)]
+    [TestCase(new[] {Signal.High, Signal.Low}, Signal.High)]
+    [TestCase(new[] {Signal.High, Signal.High}, Signal.Low)]
+    public void Example1_FlipFlop(Signal[] inputSignals, Signal outputSignal) {
+        var module = new FlipFlop("CROC", MODULE_C);
+        foreach (var inputSignal in inputSignals) {
+            module.ReceiveSignal(_context, inputSignal);
+        }
+
+        Assert.AreEqual(outputSignal, module.LastSignal);
+        
+        // this module moves the signal to the targets
+        _context.ExecuteSignalPipeline();
+        Assert.AreEqual(outputSignal, _models[MODULE_C].LastSignal);
+    }
+
+    [Test]
+    [TestCase(Signal.None, Signal.None, Signal.High)]
+    [TestCase(Signal.Low, Signal.Low, Signal.High)]
+    [TestCase(Signal.High, Signal.Low, Signal.High)]
+    [TestCase(Signal.Low, Signal.High, Signal.High)]
+    [TestCase(Signal.High, Signal.High, Signal.Low)]
+    public void Example1_Conjunction(Signal inputSignalA, Signal inputSignalB, Signal outputSignal) {
+        var moduleName = "NAND";
+
+        var testModelA = GetTestModel(MODULE_A);
+        testModelA.TargetModules = new[] {moduleName};
+        if (inputSignalA != Signal.None) {
+            testModelA.LastSignal = inputSignalA;
+        }
+
+        var testModelB = GetTestModel(MODULE_B);
+        testModelB.TargetModules = new[] {moduleName};
+        if (inputSignalB != Signal.None) {
+            testModelB.LastSignal = inputSignalB;
+        }
+
+        var module = new Conjunction(moduleName, MODULE_C);
+        module.ReceiveSignal(_context, Signal.None);
+
+        Assert.AreEqual(outputSignal, module.LastSignal);
+        
+        // this module moves the signal to the targets
+        _context.ExecuteSignalPipeline();
+        Assert.AreEqual(outputSignal, _models[MODULE_C].LastSignal);
+    }
+
+    private TestModule GetTestModel(string name) {
+        return (TestModule)_models[name];
+    }
+
+    [Test]
+    public void Example1A_PushButton() {
+        var example = new PulsePropagation(File.ReadAllLines(@"20\example1.txt"));
+
+        var result = example.PushButton();
+        Assert.AreEqual(8, result.LowSignalsSend);
+        Assert.AreEqual(4, result.HighSignalsSend);
+    }
+    
+    [Test]
+    public void Example1A_Often1() {
+        var example = new PulsePropagation(File.ReadAllLines(@"20\example1.txt"));
+
+        Assert.AreEqual(32, example.PushButtonOften(1));
+    }
+    
+    [Test]
+    public void Example1A() {
+        var example = new PulsePropagation(File.ReadAllLines(@"20\example1.txt"));
+
+        Assert.AreEqual(32_000_000, example.PushButtonOften(1000));
+    }
+    
+    [Test]
+    public void Example1B() {
+        var example = new PulsePropagation(File.ReadAllLines(@"20\example2.txt"));
+        example.Modules.Add("output", new TestModule("output"));
+
+        Assert.AreEqual(11_687_500, example.PushButtonOften(1000));
+    }
+
+    [Test]
+    public void Puzzle1() {
+        var puzzle = new PulsePropagation(File.ReadAllLines(@"20\input.txt"));
+
+        Assert.AreEqual(788_081_152, puzzle.PushButtonOften(1000));
+    }
+
+    [Test]
+    public void Puzzle2() {
+        var puzzle = new PulsePropagation(File.ReadAllLines(@"20\input.txt"));
+
+        Assert.AreEqual(788_081_152, puzzle.PushButtonUntilSand());
+    }
+}
