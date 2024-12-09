945466d632e53187dc93b239bf112b7a517a0265
slothsoft <admin@slothsoft.de>
2024-12-09 08:07:14 +0100

    day 9 part I 💾

---
+++ 2024/09/DiskFragmenter.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.day9;
+
+/// <summary>
+/// <a href="https://adventofcode.com/2024/day/9">Day 9: Disk Fragmenter</a>
+/// </summary>
+public class DiskFragmenter {
+    public DiskFragmenter(string input) {
+        Input = input.ParseDiskMap();
+    }
+
+    internal int?[] Input { get; }
+
+    public long CalculateFileSystemChecksum() {
+        var result = 0L;
+        var skipped = 0;
+        
+        for (var i = 0; i < Input.Length; i++) {
+            if (Input[i] != null) {
+                result += (i - skipped) * Input[i]!.Value;
+            } else {
+                skipped++;
+            }
+        }
+
+        return result;
+    }
+}
+
+public static class DiskFragmenterExtensions {
+    public static int?[] ParseDiskMap(this string input) {
+        var result = new List<int?>();
+        var id = 0;
+        var isFile = true;
+        
+        foreach (var c in input) {
+            var length = int.Parse(c.ToString());
+            for (var i = 0; i < length; i++) {
+                result.Add(isFile ? id : null);
+            }
+
+            if (isFile) id++;
+            isFile = !isFile;
+        }
+        
+        return result.ToArray();
+    }
+    
+    public static void Fragment(this int?[] diskMap) {
+        var firstIndex = 0;
+        var lastIndex = diskMap.Length - 1;
+
+        while (firstIndex < lastIndex) {
+            if (diskMap[firstIndex] != null) {
+                // there already is a file, so we don't do nothing
+                firstIndex++;
+                continue;
+            }
+            
+            // free space! let's find the last file part and move it
+            while (diskMap[lastIndex] == null) lastIndex--;
+
+            diskMap[firstIndex] = diskMap[lastIndex];
+            diskMap[lastIndex] = null;
+            
+            firstIndex++;
+            lastIndex--;
+        }
+    }
+    
+    public static string Stringify(this int?[] diskMap) {
+        return string.Join("", diskMap.Select(i => i == null ? "." : i.ToString()));
+    }
+}
---
+++ 2024/09/DiskFragmenterTest.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AoC.day9;
+
+public class DiskFragmenterTest {
+
+    private const string InputExample1A = "2333133121414131402";
+    private const string InputExample1B = "12345";
+    
+    [Test]
+    [TestCase(InputExample1B, "0..111....22222")]
+    [TestCase(InputExample1A, "00...111...2...333.44.5555.6666.777.888899")]
+    public void Example1_ParseInput(string input, string expectedDiskMap) {
+        var result = input.ParseDiskMap();
+        
+        Assert.NotNull(result);
+        Assert.AreEqual(expectedDiskMap,  result.Stringify());
+    }
+    
+    [Test]
+    [TestCase(InputExample1B, "022111222......")]
+    [TestCase(InputExample1A, "0099811188827773336446555566..............")]
+    public void Example1_Fragment(string input, string expectedDiskMap) {
+        var result = input.ParseDiskMap();
+        result.Fragment();
+        
+        Assert.NotNull(result);
+        Assert.AreEqual(expectedDiskMap,  result.Stringify());
+    }
+    
+    [Test]
+    public void Example1() {
+        var example = new DiskFragmenter(InputExample1A);
+        example.Input.Fragment();
+        
+        Assert.AreEqual(1928,  example.CalculateFileSystemChecksum());       
+    }
+
+    [Test]
+    public void Puzzle1() {
+        var puzzle = new DiskFragmenter(File.ReadAllLines(@"09\input.txt").Single());
+        puzzle.Input.Fragment();
+        
+        Assert.AreEqual(6_385_338_159_127,  puzzle.CalculateFileSystemChecksum());  
+    }
+
+    [Test]
+    public void Example2() {
+        var example = new DiskFragmenter(File.ReadAllLines(@"09\example.txt").Single());
+        
+        Assert.AreEqual(7,  example.CalculateFileSystemChecksum());   
+    }
+
+    [Test]
+    public void Puzzle2() {
+        var puzzle = new DiskFragmenter(File.ReadAllLines(@"09\input.txt").Single());
+        
+        Assert.AreEqual(7,  puzzle.CalculateFileSystemChecksum());  
+    }
+}
