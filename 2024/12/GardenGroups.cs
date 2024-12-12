35593953ffd11e8628881328b64a782f12796e6e
slothsoft <admin@slothsoft.de>
2024-12-12 08:34:34 +0100

    day 12 part I 🌹

---
+++ 2024/12/GardenGroups.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AoC.day12;
+
+/// <summary>
+/// <a href="https://adventofcode.com/2024/day/12">Day 12: Garden Groups</a>
+/// </summary>
+public class GardenGroups {
+    internal record Region(char plantType, IList<(int X, int Y)>  includedCoords) {
+        public char PlantType { get; } = plantType;
+        public IList<(int X, int Y)> IncludedCoords { get; } = includedCoords;
+
+        public long CalculateArea() => IncludedCoords.Count;
+        
+        public long CalculatePerimeter() {
+            var result = 0L;
+            foreach (var includedCoord in IncludedCoords) {
+                foreach (var direction in AllDirections) {
+                    var neighboringPoint = direction.Modify(includedCoord.X, includedCoord.Y);
+                    if (!IncludedCoords.Contains(neighboringPoint)) {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+    
+    internal struct Direction(string name, Func<int, int> modifyX, Func<int, int> modifyY) {
+        public (int X, int Y) Modify(int oldX, int oldY) => (
+            modifyX(oldX), 
+            modifyY(oldY)
+        );
+        public override string ToString() => name;
+    }
+
+    private static Direction[] AllDirections { get; } = [
+            new Direction("North", x => x, y => y - 1),
+            new Direction("South", x => x, y => y + 1),
+            new Direction("West", x => x - 1, y => y),
+            new Direction("East", x => x + 1, y => y),
+        ];
+    
+    public GardenGroups(IEnumerable<string> input) {
+        Input = input.ParseCharMatrix();
+    }
+
+    internal char[][] Input { get; }
+    internal Region[]? Regions { get; private set; }
+
+    public long CalculateFencingPrice() {
+        Regions ??= FetchRegions(Input);
+        return Regions.Sum(r => r.CalculateArea() * r.CalculatePerimeter());
+    }
+    
+    internal static Region[] FetchRegions(char[][] map) {
+        var result = new List<Region>();
+        var alreadyChecked = new List<(int X, int Y)>();
+        
+        for (var x = 0; x < map.Length; x++) {
+            for (var y = 0; y < map[x].Length; y++) {
+                if (!alreadyChecked.Contains((x, y))) {
+                    var regionPoints = new List<(int X, int Y)>();
+                    CollectRegionPoints(map, x, y, regionPoints);
+                    alreadyChecked.AddRange(regionPoints);
+                    result.Add(new Region(map[x][y], regionPoints));
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+    
+    private static void CollectRegionPoints(char[][] map, int startX, int startY, List<(int X, int Y)> regionPoints) {
+        var collectedRegion = map[startX][startY];
+        
+        regionPoints.Add((startX, startY));
+        foreach (var direction in AllDirections) {
+            var newPoint = direction.Modify(startX, startY);
+            if (map.IsPointOnMap(newPoint.X, newPoint.Y) && map[newPoint.X][newPoint.Y] == collectedRegion && !regionPoints.Contains(newPoint)) { 
+                CollectRegionPoints(map, newPoint.X, newPoint.Y, regionPoints);
+            }
+        }
+    }
+}
+
+public static class GardenGroupsExtensions {
+    // TODO: these could be common
+    public static bool IsPointOnMap<TTile>(this TTile[][] map, int x, int y) => IsCoordOnMap(x, map.Length) && IsCoordOnMap(y, map[0].Length);
+    private static bool IsCoordOnMap(int coord, int length) => coord >= 0 && coord < length;
+}
---
+++ 2024/12/GardenGroupsTest.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AoC.day12;
+
+public class GardenGroupsTest {
+    private const string FileExampleA = @"12\example1.txt";
+    private const string FileExampleB = @"12\example2.txt";
+    private const string FileExampleC = @"12\example3.txt";
+    
+    [Test]
+    [TestCase(FileExampleA, 'A', 0,0)]
+    [TestCase(FileExampleA, 'A', 1,0)]
+    [TestCase(FileExampleA, 'A', 2,0)]
+    [TestCase(FileExampleA, 'A', 3,0)]
+    
+    [TestCase(FileExampleA, 'B', 0,1)]
+    [TestCase(FileExampleA, 'B', 1,1)]
+    [TestCase(FileExampleA, 'C', 2,1)]
+    [TestCase(FileExampleA, 'D', 3,1)]
+    
+    [TestCase(FileExampleA, 'B', 0,2)]
+    [TestCase(FileExampleA, 'B', 1,2)]
+    [TestCase(FileExampleA, 'C', 2,2)]
+    [TestCase(FileExampleA, 'C', 3,2)]
+    
+    [TestCase(FileExampleA, 'E', 0,3)]
+    [TestCase(FileExampleA, 'E', 1,3)]
+    [TestCase(FileExampleA, 'E', 2,3)]
+    [TestCase(FileExampleA, 'C', 3,3)]
+    public void Example1_ParseInput(string file, char checkedRegion, int expectedX, int expectedY) {
+        var result = GardenGroups.FetchRegions(File.ReadAllLines(file).ParseCharMatrix());
+        
+        Assert.NotNull(result);
+
+        var region = result.Single(r => r.PlantType == checkedRegion);
+        Assert.AreEqual(true,  region.IncludedCoords.Contains((expectedX, expectedY)), string.Join("\n", region.IncludedCoords));
+    }
+    
+    [Test]
+    [TestCase(FileExampleA, 140)]
+    [TestCase(FileExampleB, 772)]
+    [TestCase(FileExampleC, 1930)]
+    public void Example1(string file, long expectedFencingPrice) {
+        var example = new GardenGroups(File.ReadAllLines(file));
+        
+        Assert.AreEqual(expectedFencingPrice,  example.CalculateFencingPrice());       
+    }
+
+    [Test]
+    public void Puzzle1() {
+        var puzzle = new GardenGroups(File.ReadAllLines(@"12\input.txt"));
+        
+        Assert.AreEqual(1522850,  puzzle.CalculateFencingPrice());  
+    }
+
+    [Test]
+    public void Example2() {
+        var example = new GardenGroups(File.ReadAllLines(@"12\example.txt"));
+        
+        Assert.AreEqual(7,  example.CalculateFencingPrice());   
+    }
+
+    [Test]
+    public void Puzzle2() {
+        var puzzle = new GardenGroups(File.ReadAllLines(@"12\input.txt"));
+        
+        Assert.AreEqual(7,  puzzle.CalculateFencingPrice());  
+    }
+}
