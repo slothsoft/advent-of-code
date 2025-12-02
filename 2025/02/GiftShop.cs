ab383856353176ebd1e57b49e929ab39b6d4650f
slothsoft <admin@slothsoft.de>
2025-12-02 07:24:23 +0100

    add day 2 🎁🏬

---
+++ 2025/02/GiftShop.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.day2;
+
+/// <summary>
+/// <a href="https://adventofcode.com/2025/day/2">Day 2: Gift Shop</a>
+/// </summary>
+public class GiftShop {
+    internal record IdRange(long Min, long Max) {
+        // so if a number repeats 6 times, does it also repeat 2 and 3 times?
+        // 12.12.12.12.12.12 => 121212.121212 / 1212.1212.1212
+        internal IEnumerable<long> FindInvalidIds(int maxRepeats = 2) {
+            for (var id = Min; id <= Max; id++) {
+                var idAsString = id.ToString();
+                var factorials = idAsString.Length.GeneratePrimeFactorials().Where(f => f <= maxRepeats).Distinct().ToArray();
+
+                foreach (var factorial in factorials) {
+                    var chunks = idAsString.SplitIntoChunks(idAsString.Length / factorial).ToArray();
+                    var differentChunks = chunks.Distinct().Count();
+                    if (chunks.Length > 1 && differentChunks == 1) {
+                        yield return id;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public GiftShop(IEnumerable<string> input) {
+        Input = ParseInput(input.Single(i => !string.IsNullOrWhiteSpace(i)));
+    }
+
+    internal IdRange[] Input { get; }
+
+    internal static IdRange[] ParseInput(string input) {
+        return input.Split(',')
+            .Select(s => s.Split('-'))
+            .Select(a => new IdRange(long.Parse(a[0]), long.Parse(a[1])))
+            .ToArray();
+    }
+
+    public long CalculatePart1() {
+        return Input.SelectMany(i => i.FindInvalidIds()).Sum();
+    }
+
+    public long CalculatePart2() {
+        return Input.SelectMany(i => i.FindInvalidIds(int.MaxValue)).Sum();
+    }
+}
+
+public static class GiftShopExtensions {
+    public static IEnumerable<int> GeneratePrimeFactorials(this int number) => ((long)number).GeneratePrimeFactorials().Select(v => (int)v);
+        
+    public static IEnumerable<long> GeneratePrimeFactorials(this long number) {
+        var currentNumber = number;
+        var maxValue = (long) Math.Sqrt(currentNumber);
+        for (var div = 2L; div <= maxValue; div++) {
+            while (currentNumber % div == 0) {
+                yield return div;
+                currentNumber /= div;
+                maxValue = currentNumber;
+            }
+        }
+
+        if (currentNumber == number) {
+            // this means nothing we ever did created a clean division, so just add the number itself
+            yield return number;
+        }
+    }
+    
+    public static IEnumerable<string> SplitIntoChunks(this string value, int chunkSize)
+    {
+        return Enumerable.Range(0, value.Length / chunkSize).Select(i => value.Substring(i * chunkSize, chunkSize));
+    }
+}
---
+++ 2025/02/GiftShopTest.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AoC.day2;
+
+public class GiftShopTest {
+    [Test]
+    [TestCase(11, 22, new long[] {11, 22})]
+    [TestCase(95, 115, new long[] {99})]
+    [TestCase(998, 1012, new long[] {1010})]
+    [TestCase(1188511880, 1188511890, new long[] {1188511885})]
+    [TestCase(222220, 222224, new long[] {222222})]
+    [TestCase(1698522, 1698528, new long[] { })]
+    [TestCase(446443, 446449, new long[] {446446})]
+    [TestCase(38593856, 38593862, new long[] {38593859})]
+    public void Example1_IdRange_FindInvalidIds(long min, long max, long[] expected) {
+        var range = new GiftShop.IdRange(min, max);
+        var result = range.FindInvalidIds().ToArray();
+        
+        Assert.NotNull(result);
+        Assert.AreEqual(expected, result);
+    }
+    
+    [Test]
+    public void Example1() {
+        var example = new GiftShop(File.ReadAllLines(@"02\example.txt"));
+        
+        Assert.AreEqual(1227775554,  example.CalculatePart1());       
+    }
+
+    [Test]
+    public void Puzzle1() {
+        var puzzle = new GiftShop(File.ReadAllLines(@"02\input.txt"));
+        
+        Assert.AreEqual(13108371860,  puzzle.CalculatePart1());  
+    }
+
+    [Test]
+    [TestCase(1, new long[] {1})]
+    [TestCase(2, new long[] {2})]
+    [TestCase(3, new long[] {3})]
+    [TestCase(4, new long[] {2, 2})]
+    [TestCase(6, new long[] {2, 3})]
+    [TestCase(8, new long[] {2, 2, 2})]
+    [TestCase(25, new long[] {5, 5})]
+    public void Example2_GeneratePrimeFactorials(long number, long[] expected) {
+        Assert.AreEqual(expected,  number.GeneratePrimeFactorials().ToArray());   
+    }
+    
+
+    [Test]
+    [TestCase("111", 1,new[] {"1", "1", "1"})]
+    [TestCase("12", 2,new[] {"12"})]
+    [TestCase("1234", 2,new[] {"12", "34"})]
+    public void Example2_SplitIntoChunks(string value, int chunkSize, string[] expected) {
+        Assert.AreEqual(expected,  value.SplitIntoChunks(chunkSize).ToArray());   
+    }
+
+    [Test]
+    public void Example2() {
+        var example = new GiftShop(File.ReadAllLines(@"02\example.txt"));
+        
+        Assert.AreEqual(4174379265,  example.CalculatePart2());   
+    }
+    
+    [Test]
+    public void Puzzle2_MaxLength() {
+        var puzzle = new GiftShop(File.ReadAllLines(@"02\input.txt"));
+        
+        Assert.AreEqual(10,  puzzle.Input.Max(r => r.Max.ToString().Length));  
+    }
+
+    [Test]
+    public void Puzzle2() {
+        var puzzle = new GiftShop(File.ReadAllLines(@"02\input.txt"));
+        
+        Assert.AreEqual(22471660255,  puzzle.CalculatePart2());  
+    }
+}
