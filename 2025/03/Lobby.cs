eaedace25f3a26eee41474e674b05ec7b91720c8
slothsoft <admin@slothsoft.de>
2025-12-03 06:14:54 +0100

    day 3 part 1 ⬆

---
+++ 2025/03/Lobby.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.day3;
+
+/// <summary>
+/// <a href="https://adventofcode.com/2025/day/3">Day 3: Lobby</a>
+/// </summary>
+public class Lobby {
+    internal record BatteryBank(params int[] Batteries) {
+        
+        internal BatteryBank(string input) : this(input.Select(b => int.Parse(b.ToString())).ToArray()) {}
+        
+        public long CalculateJoltage() {
+            var firstDigit = Batteries.Take(Batteries.Length -1).Max();
+            var firstDigitIndex = Array.IndexOf(Batteries, firstDigit);
+            var secondDigit = Batteries.Skip(firstDigitIndex + 1).Max();
+            return firstDigit * 10 + secondDigit;
+        }
+    }
+
+    public Lobby(IEnumerable<string> input) {
+        Input = ParseInput(input);
+    }
+
+    internal BatteryBank[] Input { get; }
+
+    internal static BatteryBank[] ParseInput(IEnumerable<string> input) {
+        return input.Select(s => new BatteryBank(s)).ToArray();
+    }
+
+    public long CalculateTotalJoltage() {
+        return Input.Select(i => i.CalculateJoltage()).Sum();
+    }
+}
+
+public static class LobbyExtensions {
+    public static long Calculate(this int value) {
+        return value + 1;
+    }
+}
---
+++ 2025/03/LobbyTest.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace AoC.day3;
+
+public class LobbyTest {
+    [Test]
+    [TestCase("987654321111111", 98)]
+    [TestCase("811111111111119", 89)]
+    [TestCase("234234234234278", 78)]
+    [TestCase("818181911112111", 92)]
+    public void Example1_ParseInput(string input, long expected) {
+        var batteryBank = new Lobby.BatteryBank(input);
+        Assert.AreEqual(expected,  batteryBank.CalculateJoltage());
+    }
+    
+    [Test]
+    public void Example1() {
+        var example = new Lobby(File.ReadAllLines(@"03\example.txt"));
+        
+        Assert.AreEqual(357,  example.CalculateTotalJoltage());       
+    }
+
+    [Test]
+    public void Puzzle1() {
+        var puzzle = new Lobby(File.ReadAllLines(@"03\input.txt"));
+        
+        Assert.AreEqual(17031,  puzzle.CalculateTotalJoltage());  
+    }
+
+    [Test]
+    public void Example2() {
+        var example = new Lobby(File.ReadAllLines(@"03\example.txt"));
+        
+        Assert.AreEqual(7,  example.CalculateTotalJoltage());   
+    }
+
+    [Test]
+    public void Puzzle2() {
+        var puzzle = new Lobby(File.ReadAllLines(@"03\input.txt"));
+        
+        Assert.AreEqual(7,  puzzle.CalculateTotalJoltage());  
+    }
+}
