05f20b05fe3405c4a864dc974b3fca6dac7c46ea
slothsoft <admin@slothsoft.de>
2025-12-07 06:47:45 +0100

    add day 7 part 1 & 2 💥💫

---
+++ 2025/07/Laboratories.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.day7;
+
+/// <summary>
+/// <a href="https://adventofcode.com/2025/day/7">Day 7: Laboratories</a>
+/// </summary>
+public class Laboratories {
+    public Laboratories(IEnumerable<string> input) {
+        var inputAsArray = input.ToArray();
+        Manifold = inputAsArray.ParseCharMatrix();
+        StartIndex = inputAsArray[0].IndexOf('S');
+    }
+
+    internal char[][] Manifold { get; }
+    internal int StartIndex { get; }
+
+    public long CalculateBeamSplitting() {
+        var beamSplitting = 0L;
+        CalculateBeamsOnManifold(_ => beamSplitting++);
+        return beamSplitting;
+    }
+    
+    public long CalculateTimelines() {
+        return CalculateBeamsOnManifold().Sum(xCount => xCount.Value);
+    }
+    
+    private IDictionary<int, long> CalculateBeamsOnManifold(Action<int>? onBeamSplitting = null) {
+        onBeamSplitting ??= _ => { };
+        var beamXAndCounts = new Dictionary<int, long> { {StartIndex, 1} };
+
+        for (var y = 1; y < Manifold[0].Length; y++) {
+            foreach (var (x, count) in beamXAndCounts.ToArray()) {
+                switch (Manifold[x][y]) {
+                    case '^': {
+                        // split the beam
+                        onBeamSplitting(x);
+                        beamXAndCounts.Remove(x);
+                        beamXAndCounts.AddBeam(x - 1, count);
+                        beamXAndCounts.AddBeam(x + 1, count);
+                        break;
+                    }
+                    case '.': {
+                        // nothing happens to the beam, x stays the same
+                        break;
+                    }
+                    default: throw new Exception($"Do not know how to handle field {Manifold[x][y]} ({x} | {y})");
+                    
+                }
+            }
+        }
+        
+        return beamXAndCounts;
+    }
+}
+
+public static class LaboratoriesExtensions {
+    public static void AddBeam(this IDictionary<int, long> value, int x, long count) {
+        if (!value.TryAdd(x, count)) {
+            value[x] += count;
+        }
+    }
+}
---
+++ 2025/07/LaboratoriesTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace AoC.day7;
+
+public class LaboratoriesTest {
+    [Test]
+    public void Example1() {
+        var example = new Laboratories(File.ReadAllLines(@"07\example.txt"));
+        
+        Assert.AreEqual(21,  example.CalculateBeamSplitting());       
+    }
+
+    [Test]
+    public void Puzzle1() {
+        var puzzle = new Laboratories(File.ReadAllLines(@"07\input.txt"));
+        
+        Assert.AreEqual(1516,  puzzle.CalculateBeamSplitting());  
+    }
+
+    [Test]
+    public void Example2() {
+        var example = new Laboratories(File.ReadAllLines(@"07\example.txt"));
+        
+        Assert.AreEqual(40,  example.CalculateTimelines());   
+    }
+
+    [Test]
+    public void Puzzle2() {
+        var puzzle = new Laboratories(File.ReadAllLines(@"07\input.txt"));
+        
+        Assert.AreEqual(1_393_669_447_690,  puzzle.CalculateTimelines());  
+    }
+}
